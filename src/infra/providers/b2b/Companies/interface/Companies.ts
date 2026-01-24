7c162fff02e9738e3147cd6ea489fe2bec201559
Kevin Santos Da Silva <kevinssilva1502@gmail.com>
2026-01-24 14:21:57 -0300

    refactor: alteração de companiesDeadlineType para string

--- src/infra/providers/b2b/Companies/interface/Companies.ts
+++ src/infra/providers/b2b/Companies/interface/Companies.ts
@@ -1,14 +1,12 @@
 import { CompaniesBusiness } from "../../Business/interface/Business";
 
-export type CompaniesDeadlineType = "DEFINIDO" | "INDEFINIDO";
-
 export interface Companies {
   companiesErpId: string;
   companiesKey: number;
   companiesApiKey: string;
 
   companiesLogoImg?: string;
-  companiesDeadlineType?: CompaniesDeadlineType;
+  companiesDeadlineType?: string;
   companiesTools?: string;
   companiesDefaultCarriers?: number;
 

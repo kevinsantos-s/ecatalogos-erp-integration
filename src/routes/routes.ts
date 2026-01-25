d76d2ff3c1941c998af9792cb58b96b49da32353
Kevin Santos Da Silva <kevinssilva1502@gmail.com>
2026-01-25 14:37:02 -0300

    feat: rota de branches que tinha esquecido de inserir e rota de categorias prontos no routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import { companiesRoutes } from "../modules/companies/companies.routes";
+import { categoriesRoutes } from "@/modules/categories/categories.routes";
+import { branchesRoutes } from "@/modules/branches/branches.routes";
 
 const routes = Router();
 
 routes.use("/companies", companiesRoutes);
+routes.use("/branches", branchesRoutes)
+routes.use("/categories", categoriesRoutes);
+
 
 export {routes};
